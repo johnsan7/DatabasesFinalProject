f117cd42c4a8ffca081847716f95ec92fa3e5c44
johnsan7 <johnsan7@oregonstate.edu>
2015-12-05 01:21:23 -0600

    Lots of progress

--- movieSpouseDB.php
+++ movieSpouseDB.php
@@ -423,6 +423,272 @@ $stmt->close();
 		</fieldset>
 		<input type="submit" value="Delete Religion" />
 	</form>
+
+<br>
+
+
+<!-- This draws the current film/actor relationships in a table-->
+
+<div>
+	<table>
+		<tr>
+			<td>Actors paired with films</td>
+		</tr>
+		<tr>
+			<th>Actor Name</th>
+			<th>Film Name</th>
+		</tr>
+<?php
+if(!($stmt = $mysqli->prepare("SELECT a.fname, a.lname, f.title  FROM film_actor fa INNER JOIN film f ON f.film_id = fa.fid INNER JOIN actor a ON fa.aid=a.actor_id;
+"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($fname, $lname, $title)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+ echo "<tr>\n<td>\n" . $fname . $lname . "\n</td>\n<td>\n" . $title . "\n</td>\n</tr>";
+}
+$stmt->close();
+?>
+	</table>
+</div>
+
+<br>
+<!--Add to film_actor table, two selects and for 1 post-->
+
+<div>
+	<form method="post" action="actor_film.php">
+		<fieldset>
+			<legend>Actor to pair with film</legend>
+				<select name="actorInfo">
+					<?php
+					if(!($stmt = $mysqli->prepare("SELECT actor_id, fname, lname FROM actor"))){
+						echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+					}
+					if(!$stmt->execute()){
+						echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					if(!$stmt->bind_result($aid, $afname, $alname)){
+						echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					while($stmt->fetch()){
+					 echo '<option value=" ' . $aid . ' "> ' . $aid . ' ' . $afname . ' ' . $alname .  '</option>\n';
+					}
+					$stmt->close();
+
+
+					?>
+				</select>
+		</fieldset>
+				<fieldset>
+			<legend>Film to pair with actor</legend>
+				<select name="filmInfo">
+					<?php
+					if(!($stmt = $mysqli->prepare("SELECT film_id, title FROM film"))){
+						echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+					}
+					if(!$stmt->execute()){
+						echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					if(!$stmt->bind_result($fid, $ftitle)){
+						echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					while($stmt->fetch()){
+					 echo '<option value=" ' . $fid . ' "> ' . $fid . ' ' . $ftitle .  '</option>\n';
+					}
+					$stmt->close();
+
+
+					?>
+				</select>
+		</fieldset>
+		
+		<input type="submit" value="Actor Film Pair" />
+	</form>
+</div>
+
+<!-- This draws the current film/director relationships in a table-->
+<br>
+
+<div>
+	<table>
+		<tr>
+			<td>Directors paired with films</td>
+		</tr>
+		<tr>
+			<th>Director Name</th>
+			<th>Film Name</th>
+		</tr>
+<?php
+if(!($stmt = $mysqli->prepare("SELECT d.fname, d.lname, f.title  FROM film_director fd INNER JOIN film f ON f.film_id = fd.fid INNER JOIN director d ON fd.did=d.director_id;
+"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($dfname, $dlname, $title)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+ echo "<tr>\n<td>\n" . $dfname . $dlname . "\n</td>\n<td>\n" . $title . "\n</td>\n</tr>";
+}
+$stmt->close();
+?>
+	</table>
+</div>
+
+<br>
+
+<!--Add to film_director table, two selects and for 1 post-->
+
+<div>
+	<form method="post" action="director_film.php">
+		<fieldset>
+			<legend>Director to pair with film</legend>
+				<select name="directorInfo">
+					<?php
+					if(!($stmt = $mysqli->prepare("SELECT director_id, fname, lname FROM director"))){
+						echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+					}
+					if(!$stmt->execute()){
+						echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					if(!$stmt->bind_result($did, $dfname, $dlname)){
+						echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					while($stmt->fetch()){
+					 echo '<option value=" ' . $did . ' "> ' . $did . ' ' . $dfname . ' ' . $dlname .  '</option>\n';
+					}
+					$stmt->close();
+
+
+					?>
+				</select>
+		</fieldset>
+				<fieldset>
+			<legend>Film to pair with director</legend>
+				<select name="filmInfo">
+					<?php
+					if(!($stmt = $mysqli->prepare("SELECT film_id, title FROM film"))){
+						echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+					}
+					if(!$stmt->execute()){
+						echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					if(!$stmt->bind_result($fid, $ftitle)){
+						echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					while($stmt->fetch()){
+					 echo '<option value=" ' . $fid . ' "> ' . $fid . ' ' . $ftitle .  '</option>\n';
+					}
+					$stmt->close();
+
+
+					?>
+				</select>
+		</fieldset>
+		
+		<input type="submit" value="Director Film Pair" />
+	</form>
+</div>
+
+<br>
+
+<!-- This draws the current actor marriage relationships in a table-->
+<br>
+
+<div>
+	<table>
+		<tr>
+			<td>Actors and Spouses</td>
+		</tr>
+		<tr>
+			<th>Actor Name</th>
+			<th>Spouse Name</th>
+		</tr>
+<?php
+if(!($stmt = $mysqli->prepare("SELECT a.fname, a.lname, s.fname, s.lname  FROM actor_marriage am INNER JOIN actor a ON a.actor_id = am.aid INNER JOIN spouse s ON am.sid=s.spouse_id;
+"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($afname, $alname, $sfname, $slname)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+ echo "<tr>\n<td>\n" . $afname . $alname . "\n</td>\n<td>\n" . $sfname . $slname . "\n</td>\n</tr>";
+}
+$stmt->close();
+?>
+	</table>
+</div>
+
+<br>
+
+<!--Add to actor_marriage table, two selects and for 1 post-->
+
+<div>
+	<form method="post" action="actor_marriage.php">
+		<fieldset>
+			<legend>Actor to pair with spouse</legend>
+				<select name="actorInfo">
+					<?php
+					if(!($stmt = $mysqli->prepare("SELECT actor_id, fname, lname FROM actor"))){
+						echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+					}
+					if(!$stmt->execute()){
+						echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					if(!$stmt->bind_result($aid, $afname, $alname)){
+						echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					while($stmt->fetch()){
+					 echo '<option value=" ' . $aid . ' "> ' . $aid . ' ' . $afname . ' ' . $alname .  '</option>\n';
+					}
+					$stmt->close();
+
+
+					?>
+				</select>
+		</fieldset>
+				<fieldset>
+			<legend>Spouse to pair with actor</legend>
+				<select name="spouseInfo">
+					<?php
+					if(!($stmt = $mysqli->prepare("SELECT spouse_id, fname, lname FROM spouse"))){
+						echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+					}
+					if(!$stmt->execute()){
+						echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					if(!$stmt->bind_result($sid, $sfname, $slname)){
+						echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+					}
+					while($stmt->fetch()){
+					 echo '<option value=" ' . $sid . ' "> ' . $sid . ' ' . $sfname . $slname .  '</option>\n';
+					}
+					$stmt->close();
+
+
+					?>
+				</select>
+		</fieldset>
+		
+		<input type="submit" value="Actor Marriage" />
+	</form>
 </div>
 
 
